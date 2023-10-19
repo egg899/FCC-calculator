817066316325f4d5e673476ab406c29d60e7ab3f
egg899 <germanverissimo5@hotmail.com>
2023-10-19 20:54:40 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -4,87 +4,284 @@ import './App.css'
 function App() {
   const [answer, setAnswer] = useState("0");
   const [expression, setExpression] =useState("");
-  const et = expression.trim();
+  
+  //const et = expression.trim();
 
 const isOperator = (symbol:string)=>{
   return /[*/+-]/.test(symbol);
 }
 
-const buttonPress = (symbol:string) => {
-  //console.log(symbol)
-  if(symbol ==='clear'){
-    setAnswer("");
-    setExpression("0");
-    return;
+const numbers: unknown[]=[];
+const numbersClean: unknown[]=[];
+const operators: unknown[] = [];
+const buttonPress = (symbol:string)=>{
+  
     
-  }else if(symbol === "negative"){
-    if(answer === "") return;
-    setAnswer(
-      answer.toString().charAt(0) === "-" ? answer.slice(1) : "-" + answer
-    );
-  } else if(symbol === "percent"){
-    if (answer == "") return;
-    setAnswer((parseFloat(answer)/100).toString());
-  } else if(isOperator(symbol)){
-    setExpression(et + " " + symbol + " ");
-  } else if(symbol === "="){
-    calculate()
-  }else if( symbol === "0"){
-    if(expression.charAt(0) !== "0"){
-      setExpression(expression + symbol);
-    }
-     
-  } else if(symbol === "."){
-    //split by operators and get the last number
-  const lastNumber = expression.split(/[-+*/]/g).pop();
-   
-    //if last number already has a decimel, don't add another
-  if(lastNumber?.includes(".")) return;
-  setExpression(expression + symbol);
-
-
-  }else{
-    if(expression.charAt(0) === "0"){
-      setExpression(expression.slice(1) + symbol);
-    }else{
-      setExpression(expression + symbol);
-    }
-  }
   
 
-};//buttonPress
+
+//let string = expression.split(/[-+*/]/g);
+const string = expression.split(/([-+*/=])/g)
+//let string = expression.split('');
+
+
+
+let newVal;
+let equal;
+for(let i =0; i<string.length; i++){
+
+  const parts = string[i].split('.');
+  equal = string[i].includes('=')
+  newVal = parts.shift()+ (parts.length >0 ? "." + parts.join(''):'');
+  numbers.push(newVal);
+//console.log(newVal)
+}
+if(symbol=="=" && equal){
+  return;
+}
+if(symbol=="." &&  newVal?.includes('.') ){
+  console.log('Multiple decimals');
+  return;
+}
+
+
+setExpression(expression+symbol)
+
+
+for(let i=0;i<numbers.length;i++){
+  if(numbers[i]){
+    numbersClean.push(numbers[i]);
+  }
+}
+
+console.log(numbersClean);
+console.log(expression);
+
+
+
+if(symbol=="="){
+  calculate();
+}
+
+
+
+
+
+ if(symbol =="clear"){
+  setAnswer("");
+  setExpression("0");
+  return;
+ }
+
+ 
+ else if(symbol =="negative"){
+   setAnswer( answer * (-1));
+   setExpression("");
+ }
+else if(symbol =="percentage"){
+  if(answer === ""){
+    setExpression("0");
+    return;
+  }
+  
+  else{
+    setAnswer((parseFloat(answer)/100));
+    setExpression("");
+  }
+  
+}
+
+
+ 
+ 
+ 
+  if(expression.charAt(0) === "0"){
+       setExpression(expression.slice(1) + symbol);
+     }
+
+
+
+ 
+
+
+
+
+
+}//button press
 
 const calculate = () => {
-  //if last character is an operator, do nothing
-  if(isOperator(expression.charAt(expression.length-1))) return;
-  //clean the expression so that two operators in a row uses the last operator
-  const parts = et.split(" ");
-   //console.log(parts);
-  const newParts = [];
-   for(let i=parts.length-1; i>=0; i--){
-        if(["*", "/", "+"].includes(parts[i]) && isOperator(parts[i-1])){
-          newParts.unshift(parts[i]);
-          let j=0;
-          let k= i -1;
-          while(isOperator(parts[k])){
-            k--;
-            j++;
-          }
-          i -= j;
-        } else{
-          newParts.unshift(parts[i]);
+
+  
+ //Clean the expression so that two operators in row, uses the ast operator
+
+ //it goes backward the array
+  for(let i=numbersClean.length-1; i>=0;i--){
+//it checks if the first string of the array is an operator, if it is not, it is get sent to
+//the operators array in the else satatement(operators.unshift(numbersClean[i]))
+    
+    if(["*", "/", "+"].includes(numbersClean[i]) && isOperator(numbersClean[i-1])){
+      //it checks if the following expression includes any of the operators and the isOperator function checks is the next expression is an operator
+        operators.unshift(numbersClean[i]);
+        //and then it goes to the while loops and decrements 'i' a 'j amount';
+        let j=0;
+        let k= i-1;
+        //the while loop check if there followinf expressions are operators and
+        // if they are it is going to skip them until if finds a number
+        while(isOperator(numbersClean[k])){
+          //it will count the operators and substract them, skip them
+          //and go stright to the number
+          k--;
+          j++;
         }
+        i-=j;
+    }else{
+      //here it sends the numbers once it finds out it is not an operator
+      operators.unshift(numbersClean[i]);
+    }
   }
 
-  const newExpression = newParts.join(" ");
-  if(isOperator(newExpression.charAt(0))) {
-    setAnswer(eval(answer + newExpression) as string);
-  }else{
-    setAnswer(eval(newExpression) as string);
+
+
+
+  
+  //console.log(operators);
+  if(expression.includes("=")){
+    expression.replace("=", "")
   }
+ 
+  setExpression(expression);
+
+  
+
+
+
+  setAnswer(eval(answer + operators.join("")));
   setExpression("");
+}
 
-};
+
+
+
+
+
+
+
+
+
+
+
+// const calculate = () => {
+//   //if last character is an operator, do nothing
+//   if(isOperator(expression.charAt(expression.length-1))) return;
+//   //clean the expression so that two operators in a row uses the last operator
+//   const parts = et.split(" ");
+//    //console.log(parts);
+//   const newParts = [];
+//    for(let i=parts.length-1; i>=0; i--){
+//         if(["*", "/", "+"].includes(parts[i]) && isOperator(parts[i-1])){
+//           newParts.unshift(parts[i]);
+//           let j=0;
+//           let k= i -1;
+//           while(isOperator(parts[k])){
+//             k--;
+//             j++;
+//           }
+//           i -= j;
+//         } else{
+//           newParts.unshift(parts[i]);
+//         }
+//   }
+
+//   const newExpression = newParts.join(" ");
+//   if(isOperator(newExpression.charAt(0))) {
+//     setAnswer(eval(answer + newExpression) as string);
+//   }else{
+//     setAnswer(eval(newExpression) as string);
+//   }
+//   setExpression("");
+
+// };
+
+
+
+
+
+
+//const buttonPress = (symbol:string) => {
+  //console.log(symbol)
+  // if(symbol ==='clear'){
+  //   setAnswer("");
+  //   setExpression("0");
+  //   return;
+    
+  //}else if(symbol === "negative"){
+  //   if(answer === "") return;
+  //   setAnswer(
+  //     answer.toString().charAt(0) === "-" ? answer.slice(1) : "-" + answer
+  //   );
+  // } else if(symbol === "percent"){
+  //   if (answer == "") return;
+  //   setAnswer((parseFloat(answer)/100).toString());
+  // } else if(isOperator(symbol)){
+  //   setExpression(et + " " + symbol + " ");
+  // } else if(symbol === "="){
+  //   calculate()
+  // }else if( symbol === "0"){
+  //   if(expression.charAt(0) !== "0"){
+  //     setExpression(expression + symbol);
+  //   }
+     
+  // } else if(symbol === "."){
+    //split by operators and get the last number
+  //const lastNumber = expression.split(/[-+*/]/g).pop();
+   
+    //if last number already has a decimel, don't add another
+  // if(lastNumber?.includes(".")) return;
+  // setExpression(expression + symbol);
+
+
+  // }else{
+  //   if(expression.charAt(0) === "0"){
+  //     setExpression(expression.slice(1) + symbol);
+  //   }else{
+  //     setExpression(expression + symbol);
+  //   }
+  // }
+  
+
+//};//buttonPress
+
+// const calculate = () => {
+//   //if last character is an operator, do nothing
+//   if(isOperator(expression.charAt(expression.length-1))) return;
+//   //clean the expression so that two operators in a row uses the last operator
+//   const parts = et.split(" ");
+//    //console.log(parts);
+//   const newParts = [];
+//    for(let i=parts.length-1; i>=0; i--){
+//         if(["*", "/", "+"].includes(parts[i]) && isOperator(parts[i-1])){
+//           newParts.unshift(parts[i]);
+//           let j=0;
+//           let k= i -1;
+//           while(isOperator(parts[k])){
+//             k--;
+//             j++;
+//           }
+//           i -= j;
+//         } else{
+//           newParts.unshift(parts[i]);
+//         }
+//   }
+
+//   const newExpression = newParts.join(" ");
+//   if(isOperator(newExpression.charAt(0))) {
+//     setAnswer(eval(answer + newExpression) as string);
+//   }else{
+//     setAnswer(eval(newExpression) as string);
+//   }
+//   setExpression("");
+
+// };
 
 
 
